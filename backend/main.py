f0f022089810388f306ba24adaf0807664b13ab4
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-08 08:47:56 +0330

    fastAPI-JobPosting Project: logo added to navbar using static files set up

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 import uvicorn
 
@@ -7,6 +8,7 @@ from apis.general_pages.route_homepage import general_pages_router
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 app.include_router(general_pages_router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 if __name__ == '__main__':
