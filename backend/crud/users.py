ccc7926584776b979aa4206e62766cdbf926f1f4
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-10 13:57:15 +0330

    fastAPI-JobPosting Project: user creation method added to user crud

---
+++ backend/crud/users.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+
+from models.users import User
+from core.hashing import Hasher
+
+
+def create_new_user(user, db: Session):
+    new_user = User(
+        username=user.username,
+        email = user.email,
+        hashed_password=Hasher.get_password_hash(user.password),
+        is_active=True,
+        is_superuser=False
+    )
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
