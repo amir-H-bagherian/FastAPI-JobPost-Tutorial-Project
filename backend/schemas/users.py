67baf0013dcc89f6e61da59d9914cb2ae3fd19d9
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-10 13:56:03 +0330

    fastAPI-JobPosting Project: User Response Schema completed

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -4,4 +4,12 @@ from pydantic import BaseModel, EmailStr
 class UserCreateSchema(BaseModel):
     username: str
     email: EmailStr
-    password: str
+    password: str  
+    
+class UserResponseSchema(BaseModel):
+    username: str
+    email: EmailStr
+    is_active: bool
+    
+    class Config():  #tells pydantic to convert even non dict obj to json
+        orm_mode = True
