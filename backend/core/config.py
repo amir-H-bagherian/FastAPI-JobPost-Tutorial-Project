cd7f2936b388512c794d3de643bf0c532e17ca10
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-10 11:09:02 +0330

    fastAPI-JobPosting Project: Project connected to PostgreSQL DB

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,7 +1,19 @@
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 class Settings:
     PROJECT_NAME: str = 'Job Board'
     PROJECT_VERSION: str = '1.0.0'
+    
+    POSTGRES_USER: str = os.environ['POSTGRES_USER']
+    POSTGRES_PASSWORD: str = os.environ['POSTGRES_PASSWORD']
+    POSTGRES_SERVER: str = os.environ['POSTGRES_SERVER']
+    POSTGRES_PORT: str = os.environ['POSTGRES_PORT']
+    POSTGRES_DB: str = os.environ['POSTGRES_DB']
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    
     
 settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -4,11 +4,14 @@ import uvicorn
 
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
+from db.base_class import Base
+from db.session import engine
 
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 app.include_router(general_pages_router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
+Base.metadata.create_all(bind=engine)
 
 
 if __name__ == '__main__':
