ba16889aee19cf1e73ce75d1326f20883fa45cea
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-11 09:59:57 +0330

    fastAPI-JobPosting Project: Job_posts routes completed

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ import uvicorn
 from core.config import settings
 from routes.homepage import general_pages_router
 from routes.users import user_router
+from routes.job_posts import job_post_router
 from db.base import Base
 from db.session import engine
 
@@ -12,6 +13,8 @@ from db.session import engine
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 app.include_router(general_pages_router)
 app.include_router(user_router)
+app.include_router(job_post_router)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 Base.metadata.create_all(bind=engine)
 
---
+++ backend/routes/job_posts.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from schemas.job_posts import JobCreate
+from db.session import get_db
+from crud.job_posts import create_new_job_post
+
+
+job_post_router = APIRouter(prefix='/job', tags=['job_posts'])
+
+@job_post_router.post('/')
+def create_job_post(job_post: JobCreate, db: Session = Depends(get_db)):
+    current_user = 1
+    new_job_post = create_new_job_post(new_job_post=job_post, db=db, owner_id=current_user)
+    return new_job_post
