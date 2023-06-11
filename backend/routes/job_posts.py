f7a4032cf8909f4ca8401e2afc43f61607fce291
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-11 10:04:47 +0330

    fastAPI-JobPosting Project: Job_posts create route decorator modified to have response model and create_job/ path

--- backend/routes/job_posts.py
+++ backend/routes/job_posts.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from schemas.job_posts import JobCreate
+from schemas.job_posts import JobCreate, JobShow
 from db.session import get_db
 from crud.job_posts import create_new_job_post
 
 
-job_post_router = APIRouter(prefix='/job', tags=['job_posts'])
+job_post_router = APIRouter(prefix='/jobs', tags=['job_posts'])
 
-@job_post_router.post('/')
+@job_post_router.post("/create-job/",response_model=JobShow)
 def create_job_post(job_post: JobCreate, db: Session = Depends(get_db)):
     current_user = 1
     new_job_post = create_new_job_post(new_job_post=job_post, db=db, owner_id=current_user)
