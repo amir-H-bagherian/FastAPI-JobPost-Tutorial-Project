63d3b2d13c1b4b35f0851cfb08481303c62a70f0
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-11 11:15:55 +0330

    fastAPI-JobPosting Project: Job_posts creation unit test modified

--- backend/tests/test_routes/job_posts.py
+++ backend/tests/test_routes/job_posts.py
@@ -1,8 +1,8 @@
 from fastapi import status
 
 
-def test_job_post_creation(client):
-        data = {
+def test_create_job(client):
+    data = {
         "title": "SDE super",
         "company": "doogle",
         "company_url": "www.doogle.com",
@@ -10,8 +10,8 @@ def test_job_post_creation(client):
         "description": "python",
         "date_posted": "2022-03-20"
         }
-        
-        response = client.post('/jobs/create-job/', json=data)
-        assert response.status_code == status.HTTP_200_OK 
-        assert response.json()["company"] == "doogle"
-        assert response.json()["description"] == "python"
+
+    response = client.post("/jobs/create-job/",json=data)
+    assert response.status_code == status.HTTP_200_OK 
+    assert response.json()["company"] == "doogle"
+    assert response.json()["description"] == "python"
